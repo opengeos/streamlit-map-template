f78c9413641935c315b1d11bd02684567c02122c
hechunguang <hechunguang@zju.edu.cn>
2025-06-24 16:23:04 +0200

    测试上次一个代码文件

---
+++ pages/9_📦_Charts.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
+
+all_users = ["Alice", "Bob", "Charly"]
+with st.container(border=True):
+    users = st.multiselect("用户", all_users, default=all_users)
+    rolling_average = st.toggle("Rolling average")
+
+np.random.seed(42)
+data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
+if rolling_average:
+    data = data.rolling(7).mean().dropna()
+
+tab1, tab2 = st.tabs(["Chart", "Dataframe"])
+tab1.line_chart(data, height=250)
+tab2.dataframe(data, height=250, use_container_width=True)
